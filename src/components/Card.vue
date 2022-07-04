2fcf0383ab21074bd5752853818566b2f3e88f33
Clowiser <jessica.rigaud@le-campus-numerique.fr>
2022-07-04 14:30:32 +0200

    add axios & component Card + display data & CSS

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<script>
+export default { 
+    props: ['users','namefirst', 'namelast','email', 'phone', 'img' ],
+}
+
+</script>
+
+<template>
+
+  <div class="card">
+    <img :src="img" alt="Avatar" class="img"/>
+    <div class="description">
+      <h4><b>{{namefirst}} {{namelast}} </b></h4>
+      <p>{{email}}</p>
+      <p>{{phone}}</p>
+    </div>
+  </div>
+
+
+</template>
---
+++ src/components/Index.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>Annuaire</h1>
+  <h2>Retrouvez votre correspondant !</h2>
+<hr>
+
+<div class="containercard">
+  <Card v-for="user in users" 
+  :key="user.id" 
+  :gender="user.gender" 
+  :namefirst="user.name.first" 
+  :namelast="user.name.last" 
+  :email="user.email" 
+  :phone="user.phone" 
+  :img="user.picture.large"/>
+</div>
+</template>
+
+<script >
+import Card from './Card.vue';
+import axios from 'axios';
+
+export default {
+ name: "Index",
+ components: {
+  Card
+ },
+
+data(){
+  return {
+    users: [],
+  }
+},
+
+mounted(){
+ axios.get('https://randomuser.me/api/?results=12').then(response => {
+  this.users = response.data.results
+ }).catch(function (error) {
+    //console.log(error);
+  })
+}
+
+}
+
+</script>
